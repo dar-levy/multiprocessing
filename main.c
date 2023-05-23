3ac94e94c5d9e78d8cf0d636db60a060b5555c57
Dar Levy <darlevy8@gmail.com>
2023-05-23 16:56:53 +0300

    Change skeleton to threads's

--- main.c
+++ main.c
@@ -9,11 +9,11 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <signal.h>
-#include <semaphore.h>
+#include <pthread.h>
 
 /*
  ============================================================================
- Name        : 2023_ex2_part1.c
+ Name        : 2023_ex2_part2.c
  Author      : Dar Levy
  Version     : 1
  Copyright   : Your copyright notice
@@ -21,42 +21,51 @@
  ============================================================================
  */
 
-#define SNAME "MySem"
 
 /*
- * This is a syntax assistance for part 1 question 3
- open semaphore
- sem_t *sem = sem_open(SNAME, O_CREAT, 0777, 0);
+ * this is a global variable as mentioned in recitation 5.
+ * use in part 2 question 1
+ */
+int flag = 0;
 
- increment semaphore
-sem_post(sem);
+/*
+ * This is a global integer and the syntax for mutex.
+ * use in part 2 question 2
+ * static pthread_mutex_t my_lock = PTHREAD_MUTEX_INITIALIZER;
+ * pthread_mutex_lock (&my_lock);
+ * pthread_mutex_unlock (&my_lock);
+ */
 
- decrement semaphore
-sem_wait(sem);
-*/
+int k = 0;
+
+
+
+void *threadFunc(void *vargp){
+
+    printf("No! it's not true! it's impossible\n");
+    printf("Noooooooooooooo\n");
+
+    printf("Luke, I am your father!\n");
+    printf("Search your feelings, you know it to be true.\n");
+    printf("luke, you can destroy the emperor, he has foreseen it.\n");
+    return NULL;
+}
+
 
 int main(void) {
-    int status;
-    sem_t *sem = sem_open(SNAME, O_CREAT, 0777, 0);
+    pthread_t thread_id1;
+    pthread_t thread_id2;
 
-    status = fork();
-    if (status == 0){
-        printf("No! it's not true! it's impossible\n");
-        sem_post(sem);
-        sem_wait(sem);
-        printf("Noooooooooooooo\n");
-        sem_post(sem);
-    }
-    else{
-        printf("Luke, I am your father!\n");
-        sem_wait(sem);
-        sem_post(sem);
-        printf("Search your feelings, you know it to be true.\n");
-        sem_wait(sem);
-        printf("luke, you can destroy the emperor, he has foreseen it.\n");
-    }
 
-    sem_close(sem);
+    pthread_create(&thread_id1, NULL, threadFunc, NULL);
+    pthread_create(&thread_id2, NULL, threadFunc, NULL);
+
+    pthread_join(thread_id1, NULL);
+    pthread_join(thread_id2, NULL);
+
     return 1;
 }
+
+
+
 
